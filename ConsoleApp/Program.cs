2d498d6a0f5b6b9a6d4274f9f0f24c1e406d6336
zaheersani <s@ni1981>
2018-09-11 12:41:42 +0500

    Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BCS6AVisualProgrammingDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My First C# Program!");
+            Console.WriteLine("No. of Arguments: " + args.Length);
+
+            if (args.Length > 0)
+            {
+                if (args[0] == "BCS6A" && args[1] == "SP15")
+                    Console.WriteLine("You have unlocked this class!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
