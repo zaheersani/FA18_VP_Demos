15e011f315d0c094dc6202016074273ddbba7762
zaheersani <s@ni1981>
2018-09-13 10:08:22 +0500

    FirstWinFormApp

---
+++ WinFormApp/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WinFormApp
+{
+    partial class FormCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSum = new System.Windows.Forms.Button();
+            this.txtVal1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVal2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSum
+            // 
+            this.btnSum.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSum.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSum.FlatAppearance.BorderSize = 3;
+            this.btnSum.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnSum.Location = new System.Drawing.Point(120, 51);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(68, 38);
+            this.btnSum.TabIndex = 2;
+            this.btnSum.Text = "Sum";
+            this.btnSum.UseVisualStyleBackColor = false;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // txtVal1
+            // 
+            this.txtVal1.Font = new System.Drawing.Font("Microsoft Tai Le", 10F);
+            this.txtVal1.Location = new System.Drawing.Point(59, 35);
+            this.txtVal1.Name = "txtVal1";
+            this.txtVal1.Size = new System.Drawing.Size(55, 24);
+            this.txtVal1.TabIndex = 0;
+            this.txtVal1.Leave += new System.EventHandler(this.txtVal1_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Val 1:";
+            // 
+            // txtVal2
+            // 
+            this.txtVal2.Font = new System.Drawing.Font("Microsoft Tai Le", 10F);
+            this.txtVal2.Location = new System.Drawing.Point(59, 65);
+            this.txtVal2.Name = "txtVal2";
+            this.txtVal2.Size = new System.Drawing.Size(55, 24);
+            this.txtVal2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 10F);
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Val 2:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Tai Le", 10F);
+            this.lblResult.Location = new System.Drawing.Point(56, 103);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(16, 18);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "0";
+            // 
+            // FormCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 147);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVal2);
+            this.Controls.Add(this.txtVal1);
+            this.Controls.Add(this.btnSum);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormCalc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My First WinForm App";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.TextBox txtVal1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtVal2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ WinFormApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp
+{
+    public partial class FormCalc : Form
+    {
+        /*
+         * TODO: Create Subtract, Division and Multiplication Buttons
+         * Create Event Handlers for each button and Add functionality
+        */
+        public FormCalc()
+        {
+            InitializeComponent();
+            this.txtVal1.Text = "10";
+            this.txtVal2.Text = "20";
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            //string str = "123A"
+            //int res;
+            //if (int.TryParse(str, out res))
+            //    Console.WriteLine("Parsed Val: " + res);
+            //else
+            //    Console.WriteLine("Couldn't Parsed Val: " + str);
+
+            int v1, v2;
+            if (int.TryParse(this.txtVal1.Text, out v1)
+                && int.TryParse(this.txtVal2.Text, out v2))
+            {
+                this.lblResult.Text = (v1 + v2).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Input Values are not a Number");
+            }
+        }
+
+        private void txtVal1_Leave(object sender, EventArgs e)
+        {
+            int v1;
+            if (!int.TryParse(this.txtVal1.Text, out v1))
+            {
+                this.txtVal1.Focus();
+                this.txtVal1.BackColor = Color.Red;
+            }
+            else
+            {
+                this.txtVal1.BackColor = Color.White;
+            }
+        }
+    }
+}
+
