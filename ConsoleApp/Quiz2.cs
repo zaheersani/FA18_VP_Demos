1867d97a1950b09b41593241162804b297b5aae4
zaheersani <s@ni1981>
2018-11-20 10:12:05 +0500

    LINQ to SQL Basics

---
+++ ConsoleApp/Quiz2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Create Employee class with Name, Department inline properties of type string and a List of type 
+ * Task (Task is of type Structure with Code, Priority and Description properties). 
+ * Employee class also contains an indexer which returns a Task object. 
+ * It takes Task Code as int, searches in Task list using Find method of List and returns Task object. 
+ * You are required to write C# code for Employee class only.
+ *
+*/
+
+namespace ConsoleApp
+{
+    class Employee2
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        List<Tasks> tList = new List<Tasks>();
+
+        public Tasks this[int index]
+        {
+            get
+            {
+                return this.tList.Find(t => t.Code == index);
+            }
+        }
+    }
+
+    struct Tasks
+    {
+        public int Code { get; set; }
+        public int Priority { get; set; }
+        public string Description { get; set; }
+    }
+
+}
---
+++ WpfApp/LINQDB.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for LINQDB.xaml
+    /// </summary>
+    public partial class LINQDB : Window
+    {
+        public LINQDB()
+        {
+            InitializeComponent();
+            DataClassesHospitalDBDataContext dc = new DataClassesHospitalDBDataContext();
+            //IEnumerable<user> r = from u in dc.users
+            //                      //where u.username.StartsWith("a")
+            //                      select u;
+
+            var r = from u in dc.users
+                    select new
+                    {
+                        UserID = u.Id,
+                        u.Name,
+                        UserName = u.username,
+                        u.pwd,
+                        u.email
+                    };
+            foreach (var item in r)
+            {
+                Console.WriteLine(item.UserName);
+            }
+            this.gridUsers.ItemsSource = r;
+
+            // Display Data of an entire table as it is in grid
+            //this.gridUsers.ItemsSource = dc.users;
+
+        }
+    }
+}
